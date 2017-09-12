b4c98f6f3cf505b7cb4c89cf2a101bfc937c800a
Shawn Winner <shawinne@cisco.com>
2017-09-12 11:15:48 -0700

    test

--- PycharmProjects/robot3/download_file_ssh.py
+++ PycharmProjects/robot3/download_file_ssh.py
@@ -10,6 +10,6 @@ remote_file = "/home/pi/sftp/actions.json"
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=ssh_hn,username=ssh_un,password=ssh_pw)
 
-ftp_client=ssh_client.open_sftp()
+ftp_client = ssh_client.open_sftp()
 ftp_client.get(remote_file,local_filepath)
 ftp_client.close()
